cf44f6be5080989d57d7264c98b1674ee662657f
jhw84 <jhweijs@gmail.com>
2017-02-21 12:58:06 +0100

    Initial Commit

---
+++ cpp-integration-test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  cpp-integration-test
+//
+//  Created by Joost Weijs on 21/02/2017.
+//  Copyright © 2017 Joost Weijs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
